bf4baf8b146a469acf5f6eddf1a9ed3304c052c1
AjayAR <ajayperies10@gmail.com>
2026-05-29 22:22:53 +0530

    feat: integrate Asgardeo authentication provider into the application layout

---
+++ app/components/AsgardeoProvider.tsx
@@ -0,0 +1,18 @@
+"use client"; 
+
+import { AuthProvider } from "@asgardeo/auth-react";
+const config = {
+    signInRedirectURL: "http://localhost:3000",
+    signOutRedirectURL: "http://localhost:3000",
+    clientID: "m86jbtKrG3fYAfrfRN1vrfGoR00a", 
+    baseUrl: "https://api.asgardeo.io/t/ajaypieris", 
+    scope: ["openid", "profile"] 
+}
+
+export default function AsgardeoProvider({ children }: { children: React.ReactNode }) {
+    return (
+        <AuthProvider config={config}>
+            {children}
+        </AuthProvider>
+    );
+}
